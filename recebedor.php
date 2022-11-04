5a588a7615d1e02176e092db179951ea66d50cf6
Felipe Alves <78622458+felpsalvs@users.noreply.github.com>
2022-11-04 16:02:46 -0300

    recebendo sessões de avisos

--- recebedor.php
+++ recebedor.php
@@ -1,12 +1,18 @@
 
 <?php
-$nome = filter_input(INPUT_POST, 'nome');
-$idade = filter_input(INPUT_POST, 'idade');
+session_start();
 
-if($nome && $idade) {
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+if($nome && $email && $idade) {
   echo 'NOME: '.$nome."<br/>";
+  echo 'EMAIL: '.$email."<br/>";
   echo 'IDADE: '.$idade;
 } else {
+  $_SESSION['aviso'] = 'Preencha os itens corretamente!';
+  
   header("Location: index.php");
   exit;
 }
