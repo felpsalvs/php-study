c5ce7bfba4eb033d2251b793d1f5cea4facac964
Felipe Alves <78622458+felpsalvs@users.noreply.github.com>
2022-11-09 13:11:52 +0000

    formulario criado

--- index.php
+++ index.php
@@ -3,13 +3,27 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php
+    session_start();
+    require('header.php');
 
-    <!--
-    This script places a badge on your repl's full-browser view back to your repl's cover
-    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-    teal, blue, blurple, magenta, pink!
-    -->
+    if($_SESSION['aviso']){
+      echo $_SESSION['aviso'];
+      $_SESSION['aviso'] = '';
+    }
+      ?>
+      <a href="apagar.php">Apagar cookies</a>
+    
+  <form method="post" action="recebedor.php">
+    <label>
+      Nome:
+        <input type="text" name="nome"/>
+    </label>
+    <label>
+      Email:
+        <input type="text" name="email"/>
+    </label>
+  </form>
     <script src="https://replit.com/public/js/replit-badge.js" theme="dark" defer></script> 
   </body>
 </html>
